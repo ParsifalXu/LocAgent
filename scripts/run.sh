bc2d85911f07b2048656d1c874d62a27b0c93116
ParsifalXu <43930877+ParsifalXu@users.noreply.github.com>
2025-10-27 16:48:31 +0800

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,25 +1,25 @@
 # set api key
 # or set api key in `scripts/env/set_env.sh`
 # . scripts/env/set_env.sh
-export OPENAI_API_KEY="sk-123..."
-export OPENAI_API_BASE="https://XXXXX"
+export OPENAI_API_KEY=""
+export OPENAI_API_BASE="https://api.openai.com/v1"
 
 export PYTHONPATH=$PYTHONPATH:$(pwd)
 export GRAPH_INDEX_DIR='{INDEX_DIR}/{DATASET_NAME}/graph_index_v2.3'
 export BM25_INDEX_DIR='{INDEX_DIR}/{DATASET_NAME}/BM25_index'
 
-result_path='YOUR_OUTPUT_PATH'
+result_path='/Users/esther/Desktop/projects/angel/LocAgent/outputs'
 echo $result_path
 mkdir -p $result_path
 
 python auto_search_main.py \
     --dataset 'czlll/SWE-bench_Lite' \
     --split 'test' \
-    --model 'azure/gpt-4o' \
+    --model 'gpt-4o' \
     --localize \
     --merge \
     --output_folder $result_path/location \
-    --eval_n_limit 300 \
+    --eval_n_limit 1 \
     --num_processes 50 \
     --use_function_calling \
     --simple_desc
