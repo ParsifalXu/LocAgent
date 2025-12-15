f70cf34f65a3e7bdd88009803241214bb2429e66
ParsifalXu <43930877+ParsifalXu@users.noreply.github.com>
2025-12-16 00:35:02 +0800

    update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,21 +1,24 @@
 # set api key
 # or set api key in `scripts/env/set_env.sh`
 # . scripts/env/set_env.sh
-export OPENAI_API_KEY="sk-123..."
-export OPENAI_API_BASE="https://XXXXX"
+# export OPENAI_API_KEY="sk-123..."
+# export OPENAI_API_BASE="https://XXXXX"
+
+export AWS_ACCESS_KEY_ID='..'
+export AWS_SECRET_ACCESS_KEY='..'
 
 export PYTHONPATH=$PYTHONPATH:$(pwd)
 export GRAPH_INDEX_DIR='{INDEX_DIR}/{DATASET_NAME}/graph_index_v2.3'
 export BM25_INDEX_DIR='{INDEX_DIR}/{DATASET_NAME}/BM25_index'
 
-result_path='YOUR_OUTPUT_PATH'
+result_path='/home/xuxiufeng/Projects/LocAgent/outputs'
 echo $result_path
 mkdir -p $result_path
 
 python auto_search_main.py \
     --dataset 'czlll/SWE-bench_Lite' \
     --split 'test' \
-    --model 'azure/gpt-4o' \
+    --model 'apac.anthropic.claude-3-5-sonnet-20241022-v2:0' \
     --localize \
     --merge \
     --output_folder $result_path/location \
